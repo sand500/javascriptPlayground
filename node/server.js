c65ce1758bdc3b9271f7d3b352e94b84b528090b
Sandeep Vijayasekar <sand500@gmail.com>
2015-09-26 23:06:58 -0400

    mostly working shit

--- node/server.js
+++ node/server.js
@@ -1,16 +1,22 @@
+var request = require('request');
 var http = require('http');
+
 var url = require('url');
 var fs = require('fs');
+var zlib = require('zlib');
 var server;
 
 server = http.createServer(function(req, res){
     // your normal server code
     var path = url.parse(req.url).pathname;
+    console.log(req.method);
     switch (path){
         case '/':
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.write('<h1>Hello! Try the <a href="/test.html">Test page</a></h1>');
             res.end();
+            JSON.stringify(res);
+            console.log(res);
             break;
         case '/test.html':
             fs.readFile(__dirname + path, function(err, data){
@@ -22,7 +28,11 @@ server = http.createServer(function(req, res){
                 res.end();
             });
         break;
-        default: send404(res);
+        default:
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('Fuck You ED');
+        res.end();
+
     }
 }),
 
@@ -49,7 +59,75 @@ io.sockets.on('connection', function(socket){
     //recieve client data
     socket.on('client_data', function(data){
         process.stdout.write(data.letter);
-        socket.emit('server_data', {'letter': "\n"+data.letter});
+        //socket.emit('server_data', {'letter': ""+data.letter});
+        stackSearch(data.letter,function(responeText){
+          socket.emit('server_data', {'letter': ""+responeText});  
+        })
 
     });
 });
+
+
+function stackSearch(q,callback) {
+
+  
+    var so_url = "https://api.stackexchange.com/search/advanced?order=desc&sort=activity&q="+encodeURIComponent(q)+"&filter=default&site=stackoverflow";
+
+    console.log(so_url+"\n");
+    request(so_url, {encoding: null},function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            
+
+          
+            
+            console.log(response.headers);
+            var encoding = response.headers['content-encoding']
+            if (encoding && encoding.indexOf('gzip') >= 0) {
+                  zlib.gunzip(body, function(err, dezipped) {
+                    console.log(err+" \n");
+                    var json_string = dezipped.toString('utf-8');
+                    var json = JSON.parse(json_string);
+                    
+                   // console.log(json.toString());
+                   // console.log(dezipped.toString());
+                    fs.writeFile("testTestTest", dezipped.toString(), function(error) {
+                         if (error) {
+                           console.error("write error:  " + error.message);
+                         } else {
+                           console.log("Successful Write to ");
+                         }
+                    });    
+                    callback(dezipped.toString());
+                  });
+            } else {
+              // Response is not gzipped
+            }
+
+
+            
+
+
+
+        }
+    });
+
+
+
+    /*return http.get({
+        host: 'personatestuser.org',
+        path: '/email'
+    }, function(response) {
+        // Continuously update stream with data
+        var body = '';
+        response.on('data', function(d) {
+            body += d;
+        });
+        response.on('end', function() {
+
+            // Data reception is done, do whatever with it!
+            var parsed = JSON.parse(body);
+            callback(responeText);
+        });
+    }); */
+
+}
